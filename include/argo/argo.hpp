81e1978321e4c503a31452f9ce62db8f982dce81
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-08-11 10:47:44 -0500

    Initial experiments with parsing subcommands as nested structs

---
+++ include/argo/argo.hpp
@@ -0,0 +1,48 @@
+
+#pragma once
+#include <argo/json/json.hpp>
+#include <argo/visit_struct/visit_struct.hpp>
+#include <argo/is_stl_container.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+#define ARGO_SUBCOMMAND VISITABLE_STRUCT
+#define ARGO_COMMAND VISITABLE_STRUCT
+
+namespace argo {
+
+template <typename T> void visit(T &config);
+
+struct get_argument {
+
+  // Subcommand - Nested struct
+  template <typename T>
+  inline typename std::enable_if<std::is_class<T>::value, void>::type
+  _get_argument(const char *name, T &value) {
+    std::cout << "Class type " << name << "\n";
+    visit(value);
+  }
+
+  template <typename T>
+  inline typename std::enable_if<!std::is_class<T>::value, void>::type
+  _get_argument(const char *name, T &value) {
+    std::cout << name << " " << value << "\n";
+  }
+
+  template <typename T> void operator()(const char *name, T &value) {
+    // std::string field_name = name;
+    _get_argument(name, value);
+  }
+};
+
+template <typename T> void visit(T &config) {
+  get_argument argument_visitor;
+  visit_struct::for_each(config, argument_visitor);
+}
+
+} // namespace argo
---
+++ include/argo/is_stl_container.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#include <type_traits>
+
+//specialize a type for all of the STL containers.
+namespace is_stl_container_impl{
+  template <typename T>       struct is_stl_container:std::false_type{};
+  template <typename T, std::size_t N> struct is_stl_container<std::array    <T,N>>    :std::true_type{};
+  template <typename... Args> struct is_stl_container<std::vector            <Args...>>:std::true_type{};
+  template <typename... Args> struct is_stl_container<std::deque             <Args...>>:std::true_type{};
+  template <typename... Args> struct is_stl_container<std::list              <Args...>>:std::true_type{};
+  template <typename... Args> struct is_stl_container<std::forward_list      <Args...>>:std::true_type{};
+  template <typename... Args> struct is_stl_container<std::set               <Args...>>:std::true_type{};
+  template <typename... Args> struct is_stl_container<std::multiset          <Args...>>:std::true_type{};
+  template <typename... Args> struct is_stl_container<std::map               <Args...>>:std::true_type{};
+  template <typename... Args> struct is_stl_container<std::multimap          <Args...>>:std::true_type{};
+  template <typename... Args> struct is_stl_container<std::unordered_set     <Args...>>:std::true_type{};
+  template <typename... Args> struct is_stl_container<std::unordered_multiset<Args...>>:std::true_type{};
+  template <typename... Args> struct is_stl_container<std::unordered_map     <Args...>>:std::true_type{};
+  template <typename... Args> struct is_stl_container<std::unordered_multimap<Args...>>:std::true_type{};
+  template <typename... Args> struct is_stl_container<std::stack             <Args...>>:std::true_type{};
+  template <typename... Args> struct is_stl_container<std::queue             <Args...>>:std::true_type{};
+  template <typename... Args> struct is_stl_container<std::priority_queue    <Args...>>:std::true_type{};
+}
+
+//type trait to utilize the implementation type traits as well as decay the type
+template <typename T> struct is_stl_container {
+  static constexpr bool const value = is_stl_container_impl::is_stl_container<std::decay_t<T>>::value;
+};
