7bfe1d71345a20550a8e809bba616c01ebcb0c1a
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-08-12 11:40:16 -0500

    Unit test for optional flag arguments

---
+++ test/test_optional_flag_argument.cpp
@@ -0,0 +1,25 @@
+#include <doctest.hpp>
+#include <structopt/structopt.hpp>
+
+using doctest::test_suite;
+
+struct SingleOptionalFlagArgument {
+  std::optional<bool> verbose = false;
+};
+
+STRUCTOPT(SingleOptionalFlagArgument, verbose);
+
+TEST_CASE("structopt can parse single optional argument" * test_suite("single_optional")) {
+  {
+    auto arguments = structopt::parse<SingleOptionalFlagArgument>(std::vector<std::string>{"./main", "--verbose"});
+    REQUIRE(arguments.verbose == true);
+  }
+  {
+    auto arguments = structopt::parse<SingleOptionalFlagArgument>(std::vector<std::string>{"./main", "-v"});
+    REQUIRE(arguments.verbose == true);
+  }
+  {
+    auto arguments = structopt::parse<SingleOptionalFlagArgument>(std::vector<std::string>{"./main"});
+    REQUIRE(arguments.verbose == false);
+  }
+}
