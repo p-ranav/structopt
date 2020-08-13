b3b4df46dc2e1e95c6b63a5a5571e0fc208bc48a
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-08-13 11:26:47 -0500

    Fixed duplicate symbols by making string_replace static inline

---
+++ include/structopt/string.hpp
@@ -0,0 +1,19 @@
+
+#pragma once
+#include <string>
+
+namespace structopt {
+
+namespace details {
+
+static inline bool string_replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
+}
+
+}
