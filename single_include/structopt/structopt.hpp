8916edfe8382ae760ea49ddaba321a4f66ad5bb8
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-08-14 22:00:10 -0500

    Updated single include after adding help/version optional flags

--- single_include/structopt/structopt.hpp
+++ single_include/structopt/structopt.hpp
@@ -2816,6 +2816,10 @@ public:
     // Visit the struct and save flag, optional and positional field names
     visit_struct::for_each(argument_struct, visitor);
 
+    // add `help` and `version` optional arguments
+    visitor.optional_field_names.push_back("help");
+    visitor.optional_field_names.push_back("version");
+
     // Construct the argument parser
     structopt::details::parser parser;
     parser.visitor = visitor;
@@ -2824,6 +2828,24 @@ public:
     for (std::size_t i = 1; i < parser.arguments.size(); i++) {
       parser.current_index = i;
       visit_struct::for_each(argument_struct, parser);
+    }
+
+    // directly call the parser to check for `help` and `version` flags
+    std::optional<bool> help = false, version = false;
+    for (std::size_t i = 1; i < parser.arguments.size(); i++) {
+      parser.operator()("help", help);
+      parser.operator()("version", version);
+
+      if (help == true) {
+        // if help is requested, print help and exit
+        visitor.print_help(std::cout);
+        exit(EXIT_SUCCESS);
+      }
+      else if (version == true) {
+        // if version is requested, print version and exit
+        std::cout << visitor.version << "\n";
+        exit(EXIT_SUCCESS);
+      }
     }
 
     if (!parser.visitor.positional_field_names.empty()) {
@@ -2848,10 +2870,6 @@ public:
     std::vector<std::string> arguments;
     std::copy(argv, argv + argc, std::back_inserter(arguments));
     return parse<T>(arguments);
-  }
-
-  void print_help(std::ostream& os = std::cout) const {
-    visitor.print_help(os);
   }
 };
 
